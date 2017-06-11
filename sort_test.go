3c143801c221d389886cb4c6f504ba4f3902caf9
korthaj <korthaj@users.noreply.github.com>
2017-06-11 17:42:11 +0200

    Add large benchmark

--- sort_test.go
+++ sort_test.go
@@ -1,71 +1,121 @@
 package radix
 
 import (
+	"bufio"
+	"log"
+	"os"
 	"reflect"
 	"sort"
 	"strconv"
 	"testing"
 )
 
-var text = [...]string{"", "Hello", "foo", "fo", "bar", "foo", "f00", "%*&^*&^&", "***"}
-
-var textSorted []string
-
-func init() {
-	data := text
-	textSorted = data[0:]
-	sort.Strings(textSorted)
-}
-
 func TestSort(t *testing.T) {
-	data := text
+	data := [...]string{"", "Hello", "foo", "fo", "bar", "foo", "f00", "%*&^*&^&", "***"}
+	sorted := data[0:]
+	sort.Strings(sorted)
+
 	a := data[0:]
 	Sort(a)
-	if !reflect.DeepEqual(a, textSorted) {
-		t.Errorf("sorted %v", textSorted)
-		t.Errorf("   got %v", a)
+	if !reflect.DeepEqual(a, sorted) {
+		t.Errorf(" got %v", a)
+		t.Errorf("want %v", sorted)
 	}
 }
 
 func TestSort1k(t *testing.T) {
-	a := make([]string, 1<<10)
-	for i := range a {
-		a[i] = strconv.Itoa(i ^ 0x2cc)
+	data := make([]string, 1<<10)
+	for i := range data {
+		data[i] = strconv.Itoa(i ^ 0x2cc)
 	}
-	Sort(a)
-	if !sort.StringsAreSorted(a) {
-		t.Errorf("got %v", a)
+
+	sorted := make([]string, len(data))
+	copy(sorted, data)
+	sort.Strings(sorted)
+
+	Sort(data)
+	if !reflect.DeepEqual(data, sorted) {
+		t.Errorf(" got %v", data)
+		t.Errorf("want %v", sorted)
 	}
 }
 
-func BenchmarkSortMSD1K(b *testing.B) {
+func BenchmarkSortMsdBible(b *testing.B) {
 	b.StopTimer()
-	unsorted := make([]string, 1<<10)
-	for i := range unsorted {
-		unsorted[i] = strconv.Itoa(i ^ 0x2cc)
+	var data []string
+	f, err := os.Open("res/bible.txt")
+	if err != nil {
+		log.Fatal(err)
 	}
-	data := make([]string, len(unsorted))
+	scanner := bufio.NewScanner(f)
+	for scanner.Scan() {
+		data = append(data, scanner.Text())
+	}
 
+	a := make([]string, len(data))
 	for i := 0; i < b.N; i++ {
-		copy(data, unsorted)
+		copy(a, data)
 		b.StartTimer()
-		Sort(data)
+		Sort(a)
+		b.StopTimer()
+	}
+	if err := f.Close(); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func BenchmarkSortStringsBible(b *testing.B) {
+	b.StopTimer()
+	var data []string
+	f, err := os.Open("res/bible.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	scanner := bufio.NewScanner(f)
+	for scanner.Scan() {
+		data = append(data, scanner.Text())
+	}
+
+	a := make([]string, len(data))
+	for i := 0; i < b.N; i++ {
+		copy(a, data)
+		b.StartTimer()
+		sort.Strings(a)
+		b.StopTimer()
+	}
+	if err := f.Close(); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func BenchmarkSortMsd1K(b *testing.B) {
+	b.StopTimer()
+	data := make([]string, 1<<10)
+	for i := range data {
+		data[i] = strconv.Itoa(i ^ 0x2cc)
+	}
+
+	a := make([]string, len(data))
+	for i := 0; i < b.N; i++ {
+		copy(a, data)
+		b.StartTimer()
+		Sort(a)
 		b.StopTimer()
 	}
 }
 
 func BenchmarkSortStrings1K(b *testing.B) {
 	b.StopTimer()
-	unsorted := make([]string, 1<<10)
-	for i := range unsorted {
-		unsorted[i] = strconv.Itoa(i ^ 0x2cc)
+	data := make([]string, 1<<10)
+	for i := range data {
+		data[i] = strconv.Itoa(i ^ 0x2cc)
 	}
-	data := make([]string, len(unsorted))
 
+	a := make([]string, len(data))
 	for i := 0; i < b.N; i++ {
-		copy(data, unsorted)
+		copy(a, data)
 		b.StartTimer()
-		sort.Strings(data)
+		sort.Strings(a)
 		b.StopTimer()
 	}
 }
