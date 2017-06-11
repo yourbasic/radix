b30dbe0257b77e0fc15800ea565ebb6d592a8d68
korthaj <korthaj@users.noreply.github.com>
2017-06-11 14:55:40 +0200

    Add benchmark

--- sort_test.go
+++ sort_test.go
@@ -3,6 +3,7 @@ package radix
 import (
 	"reflect"
 	"sort"
+	"strconv"
 	"testing"
 )
 
@@ -23,5 +24,48 @@ func TestSort(t *testing.T) {
 	if !reflect.DeepEqual(a, textSorted) {
 		t.Errorf("sorted %v", textSorted)
 		t.Errorf("   got %v", a)
+	}
+}
+
+func TestSort1k(t *testing.T) {
+	a := make([]string, 1<<10)
+	for i := range a {
+		a[i] = strconv.Itoa(i ^ 0x2cc)
+	}
+	Sort(a)
+	if !sort.StringsAreSorted(a) {
+		t.Errorf("got %v", a)
+	}
+}
+
+func BenchmarkSortMSD1K(b *testing.B) {
+	b.StopTimer()
+	unsorted := make([]string, 1<<10)
+	for i := range unsorted {
+		unsorted[i] = strconv.Itoa(i ^ 0x2cc)
+	}
+	data := make([]string, len(unsorted))
+
+	for i := 0; i < b.N; i++ {
+		copy(data, unsorted)
+		b.StartTimer()
+		Sort(data)
+		b.StopTimer()
+	}
+}
+
+func BenchmarkSortStrings1K(b *testing.B) {
+	b.StopTimer()
+	unsorted := make([]string, 1<<10)
+	for i := range unsorted {
+		unsorted[i] = strconv.Itoa(i ^ 0x2cc)
+	}
+	data := make([]string, len(unsorted))
+
+	for i := 0; i < b.N; i++ {
+		copy(data, unsorted)
+		b.StartTimer()
+		sort.Strings(data)
+		b.StopTimer()
 	}
 }
