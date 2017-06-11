2ad731fa550902eac667f3339d93e17bec938978
korthaj <korthaj@users.noreply.github.com>
2017-06-11 11:47:07 +0200

    Add msd radix sort

---
+++ msd.go
@@ -0,0 +1,163 @@
+package radix
+
+import "fmt"
+
+func msd(r *list, n int) (res *list) {
+	if n < 2 {
+		return r
+	}
+	var stack []frame
+	stack = append(stack, frame{
+		head: r,
+		size: n,
+	})
+	for len(stack) > 0 {
+		n := len(stack)
+		s := stack[n-1]
+		stack = stack[:n-1]
+		if s.size == 0 { // Is already sorted.
+			s.tail.next = res
+			res = s.head
+			continue
+		}
+		stack = intoBuckets(stack, s.head, s.pos)
+	}
+	return
+}
+
+type frame struct {
+	head, tail *list
+	size       int // size of list, 0 if already sorted
+	pos        int // current position in string
+}
+
+type bucket struct {
+	head, tail *list
+	size       int // size of list, 0 if already sorted
+}
+
+// DEBUG
+func printBucket(b *bucket) {
+	r := b.head
+	for {
+		fmt.Println(" ", r.str)
+		if r == b.tail {
+			break
+		}
+		r = r.next
+	}
+}
+
+// DEBUG
+func printStack(s []frame) {
+	for i, f := range s {
+		fmt.Println("frame", i)
+		r := f.head
+		for {
+			fmt.Println(" ", r.str)
+			if r == f.tail {
+				break
+			}
+			r = r.next
+		}
+	}
+}
+
+// ontoStack puts the list in a bucket onto the stack.
+// If the list contains at most insertBreak elements, its sorted
+// with insertion sort. If both the the list on top of the stack
+// and the list to be added to the stack are already sorted,
+// the new list is appended to the end of the list on the stack
+// and no new stack record is created.
+func ontoStack(stack []frame, b *bucket, pos int) []frame {
+	b.tail.next = nil
+	if b.size <= insertBreak {
+		if b.size > 1 {
+			b.head, b.tail = insertSort(b.head, pos)
+		}
+		b.size = 0 // Mark as sorted.
+	}
+	if top := len(stack) - 1; b.size == 0 && top >= 0 && stack[top].size == 0 {
+		stack[top].tail.next = b.head
+		stack[top].tail = b.tail
+	} else {
+		stack = append(stack, frame{
+			head: b.head,
+			tail: b.tail,
+			size: b.size,
+			pos:  pos,
+		})
+		b.size = 0
+	}
+	b.head = nil
+	return stack
+}
+
+// intoBuckets traverses a list and puts the elements into buckets
+// according to the character in position pos. The elements are moved
+// in blocks consisting of strings that have a common character
+// in position pos. We keep track of the minimum and maximum characters
+// encountered. In this way we may avoid looking at some empty buckets
+// when we traverse the buckets in order and push the lists onto the stack.
+func intoBuckets(stack []frame, a *list, pos int) []frame {
+	b := make([]bucket, 257) // b[256] holds strings shorter than pos.
+	chMin, chMax := 255, 0
+
+	t := a
+	prevCh := 256
+	if len(t.str) > pos {
+		prevCh = int(t.str[pos])
+	}
+	size := 1
+	for tn := t.next; tn != nil; t, tn = tn, tn.next {
+		ch := 256
+		if len(tn.str) > pos {
+			ch = int(tn.str[pos])
+		}
+		size++
+		if ch == prevCh {
+			continue
+		}
+		intoBucket(&b[prevCh], a, t, size-1, prevCh, &chMin, &chMax)
+		a = tn
+		prevCh = ch
+		size = 1
+	}
+	intoBucket(&b[prevCh], a, t, size, prevCh, &chMin, &chMax)
+
+	if b[256].head != nil {
+		b[256].size = 0 // Mark as already sorted.
+		stack = ontoStack(stack, &b[256], pos)
+	}
+	for i, max := int(chMin), int(chMax); i <= max; i++ {
+		if b[i].head != nil {
+			stack = ontoStack(stack, &b[i], pos+1)
+		}
+	}
+	return stack
+}
+
+// intoBucket puts a list of elements into a bucket.
+// The minimum and maximum character seen so far (chMin, chMax)
+// are updated when the bucket is updated for the first time.
+func intoBucket(b *bucket, head, tail *list, size int,
+	ch int, chMin, chMax *int) {
+	if b.head != nil {
+		b.tail.next = head
+		b.tail = tail
+		b.size += size
+		return
+	}
+	b.head = head
+	b.tail = tail
+	b.size = size
+	if ch == 256 {
+		return
+	}
+	if ch < *chMin {
+		*chMin = ch
+	}
+	if ch > *chMax {
+		*chMax = ch
+	}
+}
--- sort.go
+++ sort.go
@@ -8,6 +8,7 @@ func Sort(a []string) {
 	if n < 2 {
 		return
 	}
+	// Build a linked list.
 	mem := make([]list, n)
 	for i, s := range a {
 		mem[i].str = s
@@ -15,11 +16,13 @@ func Sort(a []string) {
 			mem[i].next = &mem[i+1]
 		}
 	}
-	r, _ := insertSort(&mem[0], 0)
-	//r := msd(&mem[0], n)
+
+	res := msd(&mem[0], n)
+
+	// Put elements back into slice.
 	for i := range a {
-		a[i] = r.str
-		r = r.next
+		a[i] = res.str
+		res = res.next
 	}
 }
 
@@ -32,136 +35,9 @@ func SortByte(b []byte) {
 
 const insertBreak = 16
 
-type bucket struct {
-	head, tail *list
-	size       int // size of list, 0 if already sorted
-}
-
-type frame struct {
-	head, tail *list
-	size       int // size of list, 0 if already sorted
-	pos        int // current position in string
-}
-
 type list struct {
 	str  string
 	next *list
-}
-
-// ontoStack puts the list in a bucket onto the stack.
-// If the list contains at most insertBreak elements, its sorted
-// with insertion sort. If both the the list on top of the stack
-// and the list to be added to the stack are already sorted,
-// the new list is appended to the end of the list on the stack
-// and no new stack record is created.
-func ontoStack(b *bucket, stack []frame, pos int) {
-	b.tail.next = nil
-	if b.size <= insertBreak {
-		if b.size > 1 {
-			b.head, b.tail = insertSort(b.head, pos)
-		}
-		b.size = 0 // Mark as sorted.
-	}
-	if b.size == 0 || len(stack) == 0 || stack[len(stack)-1].size == 0 {
-		stack = append(stack, frame{
-			head: b.head,
-			tail: b.tail,
-			size: b.size,
-			pos:  pos,
-		})
-	} else {
-		top := stack[len(stack)-1]
-		top.tail.next = b.head
-		top.tail = b.tail
-	}
-	b.head = nil
-}
-
-// intoBucket puts a list of elements into a bucket.
-func intoBucket(b *bucket, head, tail *list, size int, ch byte) {
-	if b.head == nil {
-		b.head = head
-		b.tail = tail
-		b.size = size
-	} else {
-		b.tail.next = head
-		b.tail = tail
-		b.size += size
-	}
-}
-
-// intoBuckets traverses a list and puts the elements into buckets
-// according to the character in position pos. The elements are moved
-// in blocks consisting of strings that have a common character
-// in position pos. We keep track of the minimum and maximum nonzero
-// characters encountered. In this way we may avoid looking at some
-// empty buckets when we traverse the buckets in ascending order
-// and push the lists onto the stack.
-func intoBuckets(a *list, stack []frame, pos int) {
-	b := make([]bucket, 256)
-	chMin, chMax := byte(255), byte(0)
-	size := 1
-	t := a
-	prevCh := t.str[pos]
-	for tn := t.next; tn != nil; t, tn = tn, tn.next {
-		ch := tn.str[pos]
-		size++
-		if ch == prevCh {
-			continue
-		}
-		intoBucket(&b[prevCh], a, t, size-1, prevCh)
-		if prevCh <= chMin {
-			chMin = prevCh
-		}
-		if prevCh >= chMax {
-			chMax = prevCh
-		}
-		a = tn
-		prevCh = ch
-		size = 1
-	}
-	intoBucket(&b[prevCh], a, t, size, prevCh)
-	if prevCh < chMin {
-		chMin = prevCh
-	}
-	if prevCh > chMax {
-		chMax = prevCh
-	}
-
-	// TODO Fix end-of-string.
-	if b[0].head != nil {
-		if b[0].size == 0 { // Mark as already sorted.
-			//ontoStack(b, stack, pos)
-		}
-	}
-	for i := chMin; i <= chMax; i++ {
-		if b[i].head != nil {
-			//ontoStack(i, stack, pos+1)
-		}
-	}
-}
-
-func msd(r *list, n int) (res *list) {
-	if n < 2 {
-		return
-	}
-	var stack []frame
-	stack = append(stack, frame{
-		head: r,
-		size: n,
-	})
-	for len(stack) > 0 {
-		n := len(stack)
-		s := stack[n-1]
-		stack = stack[:n-1]
-		if s.size == 0 { // Is already sorted.
-			s.tail.next = res
-			res = s.head
-			continue
-		}
-		intoBuckets(s.head, stack, s.pos)
-	}
-	return
 }
 
 // insertSort sorts r comparing strings starting at position p.
